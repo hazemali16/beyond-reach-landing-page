0556320b95b7ae18303fb590767111a31c1926e4
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-08-07 16:45:41 +0300

    Update Services.jsx

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -3,7 +3,7 @@ import services_image from "../images/marketing.png"
 
 const Services = () => {
   return (
-  <div className="w-10/12 flex justify-center gap-12 items-center mx-auto opacity-0 duration-500 translate-y-10 max-sm:w-11/12 max-lg:flex-col-reverse" id="services">
+  <div className="w-10/12 flex justify-center gap-12 items-center mx-auto opacity-0 duration-500 translate-y-10 max-sm:w-11/12 max-lg:flex-col-reverse pt-40" id="services">
     <div className="w-2/5 overflow-hidden max-lg:w-full">
     <h2 className="text-3xl mb-7 font-bold text-[var(--second-color)] duration-500 translate-y-[200px] delay-300 max-sm:text-2xl max-sm:mb-5">التسويق الأدائي</h2>
     <p className="text-lg leading-relaxed duration-700 translate-y-[200px] delay-700 max-sm:text-base">
