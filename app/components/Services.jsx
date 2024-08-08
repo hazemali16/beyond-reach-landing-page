67d17705edb46b0b1aaf07bdf85355b48a6e12bd
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-08-08 12:14:58 +0300

    Update Services.jsx

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -7,9 +7,9 @@ const Services = () => {
     <div className="w-2/5 overflow-hidden max-lg:w-full">
     <h2 className="text-3xl mb-7 font-bold text-[var(--second-color)] duration-500 translate-y-[200px] delay-300 max-sm:text-2xl max-sm:mb-5">التسويق الأدائي</h2>
     <p className="text-lg leading-relaxed duration-700 translate-y-[200px] delay-700 max-sm:text-base">
-    نحن في بيوند ريتش نعتبر التسويق األدائي هو قلب خدماتنا.
-باستخدام استراتيجيات مبتكرة وتحليالت دقيقة، نحقق نتائج قابلة للقياس ونقدم عائد استثماري ملموس من الحمالت
-التسويقية األدائية الفريدة.
+    نحن في بيوند ريتش نعتبر التسويق الأدائي هو قلب خدماتنا.
+باستخدام استراتيجيات مبتكرة وتحليالت دقيقة، نحقق نتائج قابلة للقياس ونقدم عائد استثماري ملموس من الحملات
+التسويقية الأدائية الفريدة.
     </p>
     </div>
     <div className="w-2/5 max-lg:w-full">
